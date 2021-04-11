536397cfae4b3729e4db8dd9b7f1a497d9f648ed
NellyD3v <not-a-real-email@naomi.life>
2021-04-11 22:09:46 +0700

    Replaced tabs with spaces.

--- src/ru/nellyd3v/mentioner/Main.java
+++ src/ru/nellyd3v/mentioner/Main.java
@@ -3,9 +3,9 @@ package ru.nellyd3v.mentioner;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	public void onEnable() {
-		this.saveDefaultConfig();
-		getServer().getPluginManager().registerEvents(new Events(this), this);
-		System.out.println("Hello from Mentioner!!!");
-	}
+    public void onEnable() {
+        this.saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new Events(this), this);
+        System.out.println("Hello from Mentioner!!!");
+    }
 }
